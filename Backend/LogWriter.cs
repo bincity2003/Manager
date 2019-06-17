ada73d3b06c80f5a07fcb6d957f37b0123f03cfe
Binh Nguyen (Work) <bincity2003@outlook.com>
2019-06-17 17:06:20 +0700

    initial commit for LogWriter

---
+++ Backend/LogWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Backend
+{
+    public static class LogWriter
+    {
+        public static void Write(string path, string data, bool includeTime)
+        {
+            if (includeTime)
+            {
+                data = $"{DateTime.Now}: " + data;
+            }
+            File.AppendAllText(path, data + "\n");
+        }
+    }
+}
